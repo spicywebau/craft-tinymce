4b1eaef79e434f70c07db9b92dc6d9f3f3a8f613
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-09-02 16:01:20 +1000

    Now installable on Craft 4

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,68 +1,23 @@
 <?php
-namespace Craft;
 
-class TinyMCEPlugin extends BasePlugin
+namespace spicyweb\tinymce;
+
+use craft\base\Plugin as BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @package spicyweb\tinymce
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ * @since 0.1.0
+ */
+class Plugin extends BasePlugin
 {
-	public function getName()
-	{
-		return "TinyMCE";
-	}
-
-	public function getDescription()
-	{
-		return Craft::t("Adds TinyMCE as a rich text field type to Craft");
-	}
-
-	public function getVersion()
-	{
-		return '1.0.0';
-	}
-
-	public function getSchemaVersion()
-	{
-		return '1.0.0';
-	}
-
-	public function getDeveloper()
-	{
-		return "Spicy Web";
-	}
-
-	public function getDeveloperUrl()
-	{
-		return "http://spicyweb.com.au";
-	}
-
-	public function getMinCraftVersion()
-	{
-		return '2.5';
-	}
-
-	public function getMinPHPVersion()
-	{
-		return '5.4';
-	}
-
-	public function isCompatible()
-	{
-		return (
-			version_compare(craft()->getVersion(), $this->getMinCraftVersion(), '>=') &&
-			version_compare(PHP_VERSION, $this->getMinPHPVersion(), '>=')
-		);
-	}
-
-	public function onBeforeInstall()
-	{
-		$compatible = $this->isCompatible();
-
-		if(!$compatible)
-		{
-			self::log(Craft::t("This plugin requires at least Craft {craft} and PHP {php}", [
-				'craft' => craft()->getVersion(),
-				'php' => PHP_VERSION,
-			]), LogLevel::Error, true);
-		}
-
-		return $compatible;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        parent::init();
+    }
 }
