31369f21c87d941ce757d8ede7a9598de0257b91
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-09-11 14:21:42 +1000

    Add English translation file

---
+++ src/translations/en/tinymce.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Cancel' => 'Cancel',
+    'Caption' => 'Caption',
+    'Insert an image' => 'Insert an image',
+    'Insert/edit link' => 'Insert/edit link',
+    'Link' => 'Link',
+    'Link to a category' => 'Link to a category',
+    'Link to a product' => 'Link to a product',
+    'Link to a variant' => 'Link to a variant',
+    'Link to an asset' => 'Link to an asset',
+    'Link to an entry' => 'Link to an entry',
+    'No transform' => 'No transform',
+    'Open in new tab?' => 'Open in new tab?',
+    'Save' => 'Save',
+    'Site' => 'Site',
+    'Text' => 'Text',
+    'TinyMCE' => 'TinyMCE',
+    'Title' => 'Title',
+    'Transform' => 'Transform',
+    'URL' => 'URL',
+];
