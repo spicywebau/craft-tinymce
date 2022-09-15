1d0da83acfd27bd9419d51851d0f6815151ad23d
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-09-15 16:03:34 +1000

    P&T credits

--- src/fields/TinyMCE.php
+++ src/fields/TinyMCE.php
@@ -1,5 +1,34 @@
 <?php
 
+/*
+This class is based on the Redactor field class from the Redactor plugin version 3.0.2, by Pixel & Tonic, Inc.
+https://github.com/craftcms/redactor/blob/3.0.2/src/Field.php
+The Redactor plugin is released under the terms of the MIT License, a copy of which is included below.
+https://github.com/craftcms/redactor/blob/3.0.2/LICENSE.md
+
+The MIT License (MIT)
+
+Copyright (c) Pixel & Tonic, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 namespace spicyweb\tinymce\fields;
 
 use Craft;
@@ -18,6 +47,14 @@ use spicyweb\tinymce\assets\FieldAsset;
 use spicyweb\tinymce\assets\TinyMCEAsset;
 use spicyweb\tinymce\Plugin;
 
+/**
+ * Class TinyMCE
+ *
+ * @package spicyweb\tinymce\fields
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 1.0.0
+ */
 class TinyMCE extends HtmlField
 {
     /**
