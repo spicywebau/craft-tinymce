5451d6aee3123ab6c8a13fd4aa74e564694977ce
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-09-15 16:07:52 +1000

    Register the translations so far

--- src/assets/FieldAsset.php
+++ src/assets/FieldAsset.php
@@ -30,4 +30,37 @@ class FieldAsset extends AssetBundle
 
         parent::init();
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function registerAssetFiles($view): void
+    {
+        $view->registerTranslations('tinymce', [
+            'Cancel',
+            'Caption',
+            'Edit image',
+            'Edit link',
+            'Insert an image',
+            'Insert/edit link',
+            'Link',
+            'Link to a category',
+            'Link to a product',
+            'Link to a variant',
+            'Link to an asset',
+            'Link to an entry',
+            'No transform',
+            'Open in new tab?',
+            'Save',
+            'Site',
+            'Text',
+            'There was an error generating the transform URL.',
+            'TinyMCE',
+            'Title',
+            'Transform',
+            'URL',
+        ]);
+
+        parent::registerAssetFiles($view);
+    }
 }
