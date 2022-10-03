c5934f94a3e3f7b312f008a603fe7ae6f92121f4
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-10-03 11:40:45 +1100

    Changes to which config settings are saved in json files

--- src/services/ConfigService.php
+++ src/services/ConfigService.php
@@ -25,32 +25,11 @@ class ConfigService extends Component
     public function generateDefault(array $overrides = []): array
     {
         return $overrides + [
-            'skin' => 'craft',
             'plugins' => 'autoresize lists link image code',
-            'content_css' => false,
-
-            // Toolbars
             'menubar' => false,
             'statusbar' => false,
             'toolbar' => 'undo redo | blocks | bold italic strikethrough | bullist numlist | insertLink insertImage | hr | code',
-
-            // Context menu (the default setting, except replacing the inbuilt link/image options with our own)
             'contextmenu' => 'craftLink linkchecker craftImage table spellchecker configurepermanentpen',
-
-            // Formatting
-            'allow_conditional_comments' => false,
-            'element_format' => 'xhtml',
-            'entity_encoding' => 'raw',
-            'fix_list_elements' => true,
-
-            // Links
-            'relative_urls' => false,
-            'remove_script_host' => false,
-            'anchor_top' => false,
-            'anchor_bottom' => false,
-
-            // Auto-resize
-            'autoresize_bottom_margin' => 0,
         ];
     }
 
@@ -62,10 +41,13 @@ class ConfigService extends Component
      */
     public function generateSimple(array $overrides = []): array
     {
-        return $this->generateDefault($overrides + [
+        return $overrides + [
+            'plugins' => 'autoresize lists',
+            'menubar' => false,
+            'statusbar' => false,
             'toolbar' => 'bold italic',
             'contextmenu' => 'table spellchecker configurepermanentpen',
-        ]);
+        ];
     }
 
     /**
