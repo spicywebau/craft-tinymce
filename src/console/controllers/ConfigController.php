23a7c075669ab5616303652f1e291bc4256f7aeb
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-10-09 20:03:26 +1100

    Add plugins/menubar/statusbar/toolbar/contextmenu options to console commands

--- src/console/controllers/ConfigController.php
+++ src/console/controllers/ConfigController.php
@@ -17,13 +17,53 @@ use yii\console\ExitCode;
 class ConfigController extends Controller
 {
     /**
+     * @var string|null The TinyMCE plugin override items to include.
+     */
+    public ?string $plugins = null;
+
+    /**
+     * @var string|null The TinyMCE menu bar override items to include, or '1' for all items, or '0' for none.
+     */
+    public ?string $menubar = null;
+
+    /**
+     * @var bool|null The TinyMCE status bar override setting, to override the chosen config.
+     */
+    public ?bool $statusbar = null;
+
+    /**
+     * @var string|null The TinyMCE toolbar override items to include.
+     */
+    public ?string $toolbar = null;
+
+    /**
+     * @var string|null The TinyMCE context menu override items to include.
+     */
+    public ?string $contextmenu = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function options($actionID): array
+    {
+        $options = parent::options($actionID);
+        $options[] = 'plugins';
+        $options[] = 'menubar';
+        $options[] = 'statusbar';
+        $options[] = 'toolbar';
+        $options[] = 'contextmenu';
+
+        return $options;
+    }
+
+    /**
      * Regenerates the Default.json config file.
      *
      * @return int
      */
     public function actionRegenerateDefault(): int
     {
-        return $this->_saveConfig('Default', Plugin::$plugin->config->generateDefault());
+        return $this->_saveConfig('Default');
     }
 
     /**
@@ -33,7 +73,7 @@ class ConfigController extends Controller
      */
     public function actionRegenerateFull(): int
     {
-        return $this->_saveConfig('Full', Plugin::$plugin->config->generateFull());
+        return $this->_saveConfig('Full');
     }
 
     /**
@@ -43,15 +83,35 @@ class ConfigController extends Controller
      */
     public function actionRegenerateSimple(): int
     {
-        return $this->_saveConfig('Simple', Plugin::$plugin->config->generateSimple());
+        return $this->_saveConfig('Simple');
     }
 
     /**
      * General method for regenerating TinyMCE field configs.
      */
-    private function _saveConfig(string $filename, array $config): int
+    private function _saveConfig(string $filename): int
     {
         $this->stdout("Saving $filename.json..." . PHP_EOL);
+        $method = "generate$filename";
+        $overrides = [];
+
+        foreach (['plugins', 'statusbar', 'toolbar', 'contextmenu'] as $property) {
+            $value = $this->{$property};
+
+            if ($value !== null) {
+                $overrides[$property] = $value;
+            }
+        }
+
+        if ($this->menubar !== null) {
+            $overrides['menubar'] = match ($this->menubar) {
+                '1' => true,
+                '0' => false,
+                default => $this->menubar,
+            };
+        }
+
+        $config = Plugin::$plugin->config->{$method}($overrides);
 
         try {
             Plugin::$plugin->config->save($filename, $config);
